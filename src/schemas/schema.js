56b15e87449181bd9e86db12c7aee17374fe6dec
Atul Verma <atulverma82@gmail.com>
2017-12-02 20:31:20 +1100

    adding first cut for asset request schema

--- src/schemas/schema.js
+++ src/schemas/schema.js
@@ -16,3 +16,43 @@ exports.SHOPPING_CENTRE_REQUEST = s({
     country: s.value('AUS')
   }
 });
+
+
+exports.ASSET_REQUEST = s({
+  name: 'string',
+  status: s.enum({
+    values: ['Active', 'Inactive']
+  }),
+
+  screenDimensions: {
+    height: 'number',
+    width: 'number',
+  },
+
+  adSpotLength: 'number',
+  maxAdvertisers: 'number',
+  maxAdLoopTime: {
+    value: 'number',
+    unit: 'string'
+  },
+  dailyFootfall: 'number',
+  beaconRFIDEnabled: s.optional('boolean'),
+  wallMounted: s.optional('boolean'),
+  interactive: s.optional('boolean'),
+  floorStanding: s.optional('boolean'),
+
+  location: {
+    floor: 'number',
+    mainCorridor: 'boolean',
+    isParking: 'boolean',
+    isFoodCourt: 'boolean',
+    nearShop: s.optional('boolean'),
+    shopName: s.optional('string'),
+    shopNumber: s.optional('string'),
+    nearEntryGate: s.optional('boolean'),
+    nearExitGate: s.optional('boolean'),
+    gateNumber: s.optional('string'),
+    pillarNumber: s.optional('string'),
+    description: s.optional('string')
+  }
+});
